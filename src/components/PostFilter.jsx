dabb062c1b75bc7cc3d54b24163d74e58f93855f
bobdm4 <asdf514asdf514@gmail.com>
2022-03-02 17:37:39 +0200

    add search and filter

---
+++ src/components/PostFilter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { MyInput } from './../UI/input/MyInput'
+import { MySelect } from './../UI/select/MySelect'
+
+export const PostFilter = ({ filter, setFilter }) => {
+  return (
+    <div>
+      <MyInput value={filter.query} onChange={e => setFilter({ ...filter, query: e.target.value })} placeholder='Поиск...' />
+      <MySelect
+        value={filter.sort}
+        onChange={selectedSort => setFilter({ ...filter, sort: selectedSort })}
+        defautValue='Сортировка по'
+        options={[
+          { value: 'title', name: 'По названию' },
+          { value: 'body', name: 'По описанию' },
+        ]}
+      />
+    </div>
+  )
+}
